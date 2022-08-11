d32253d4cbd14ba5fca0ff5cd649b8534e18618b
lPedru <dejesus.pedro2002@gmail.com>
2022-08-10 21:47:37 -0300

    Finished Section 13

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,42 +6,81 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
-
-const launch = {
-  flightNumber: 100, // flight_number
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1', // rocket.name
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'],
-  upcoming: true,
-  success: true
-}
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchesData() {
+async function populateLaunches() {
   console.log('Downloading launches data...');
-  await axios.post(SPACEX_API_URL, {
+  const response = await axios.post(SPACEX_API_URL, {
     query: {},
     options: {
+      pagination: false,
       populate: [
         {
           path: 'rocket',
           select: {
             name: 1
           }
+        },
+        {
+          path: 'payloads',
+          select: {
+            'customers': 1
+          }
         }
       ]
     }
   });
+
+  if (response.status !== 200) {
+    console.log('Problem downloading launch data');
+    throw new Error('launch data download failed');
+  }
+
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap( payload => {
+      return payload['customers'];
+    });
+    
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket'] ['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers,
+    };
+
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+
+    await saveLaunch(launch);
+  }
 }
 
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: 'Falcon 1',
+    mission: 'FalconSat',
+  });
+
+  if (firstLaunch) {
+    console.log('Launch data already loaded!');
+  } else {
+    await populateLaunches();
+  }
+
+  
+}
+
+async function findLaunch(filter) {
+  return await launchesDB.findOne(filter);
+} 
+
 async function existsLaunchWithId(launchId) {
-  return await launchesDB.findOne({
+  return await findLaunch({
     flightNumber: launchId,
   });
 }
@@ -58,23 +97,27 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDB.find({}, { '_id': 0, '__v': 0 });
+async function getAllLaunches(skip, limit) {
+  return await launchesDB
+  .find({}, { '_id': 0, '__v': 0 })
+  .sort({ flightNumber: 1 })
+  .skip(skip)
+  .limit(limit);
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if(!planet) { throw new Error('No matching planet was Found'); }
-
   await launchesDB.findOneAndUpdate({
     flightNumber: launch.flightNumber,
   }, launch, { upsert: true }) 
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if(!planet) { throw new Error('No matching planet was Found'); }
+
   const newFlightNumber = await getLatestFlightNumber() + 1;
 
   const newLaunch = Object.assign(launch, {
@@ -103,7 +146,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
-  loadLaunchesData,
+  loadLaunchData,
   getAllLaunches,
   scheduleNewLaunch,
   existsLaunchWithId,
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:pTE4Ol1FKoSBd8gx@nodecourse.ucwn8ys.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
